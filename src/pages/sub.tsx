ef7b6bcd2349076b6bcfdfca6c4666721c1102ad
nahyunee96 <108210796+nahyunee96@users.noreply.github.com>
2023-08-21 13:45:06 +0900

    Update!

--- src/pages/sub.tsx
+++ src/pages/sub.tsx
@@ -1,5 +1,5 @@
-import gsap from "gsap"
-import ScrollTrigger from "gsap/ScrollTrigger"
+import gsap from "gsap/dist/gsap"
+import ScrollTrigger from "gsap/dist/ScrollTrigger"
 import { useRef, useEffect } from "react"
 import { Inter } from 'next/font/google'
 import styles from '../styles/scroll.module.scss'
