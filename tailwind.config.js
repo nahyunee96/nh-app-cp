400112cbabe78fc34d32a9b02bef12cfaf34a7e5
nahyunee96 <108210796+nahyunee96@users.noreply.github.com>
2023-08-24 14:56:14 +0900

    Update!

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      translate: {
+        '-200': '-200px', 
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
