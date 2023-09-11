d13b62be38bb55193b3af321cd53fe6deaee39c7
nahyunee96 <108210796+nahyunee96@users.noreply.github.com>
2023-09-11 10:15:04 +0900

    Upload!

--- src/hooks/head.ts
+++ src/hooks/head.ts
@@ -1,31 +1,32 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
+import styles from '../styles/main.module.scss'
 import gsap from 'gsap'
 
 const useHeadScroll = (wrapperRef: React.RefObject<HTMLDivElement>) => {
-  const [isHeaderVisible, setHeaderVisible] = useState<boolean>(false);
+  const [isHeaderVisible, setHeaderVisible] = useState<boolean>(false)
 
   const handleScroll = () => {
-    const section02 = document.getElementById('section02');
-    const section02Top = section02?.offsetTop || 0;
-    const scrollY = window.scrollY;
+    const section02 = document.getElementById('section02')
+    const section02Top = section02?.offsetTop || 0
+    const scrollY = window.scrollY
 
     // 헤더가 나타날 조건: window.scrollTop이 section02 상단에 닿았을 때
     if (scrollY >= section02Top) {
-      setHeaderVisible(true);
+      setHeaderVisible(true)
     } else {
-      setHeaderVisible(false);
+      setHeaderVisible(false)
     }
-  };
+  }
 
   useEffect(() => {
     // 스크롤 이벤트 리스너 추가
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll)
 
     // 컴포넌트 언마운트 시 이벤트 리스너 제거
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   useEffect(() => {
     if (isHeaderVisible) {
@@ -34,39 +35,41 @@ const useHeadScroll = (wrapperRef: React.RefObject<HTMLDivElement>) => {
         '.header',
         { opacity: 0, y: '-100%' }, // 시작 상태
         { opacity: 1, y: '0%', duration: 0.5, ease: 'power3.inOut' } // 종료 상태
-      );
+      )
     } else {
       // 헤더가 사라질 때의 애니메이션 처리
-      gsap.to('.header', { opacity: 0, y: '-100%', duration: 0.5, ease: 'power3.inOut' });
+      gsap.to('.header', { opacity: 0, y: '-100%', duration: 0.5, ease: 'power3.inOut' })
     }
-  }, [isHeaderVisible]);
+  }, [isHeaderVisible])
 
   useEffect(() => {
     if (wrapperRef.current) {
-      let ON_1_ADD: NodeJS.Timeout | undefined;
-      let ON_2_ADD: NodeJS.Timeout | undefined;
-      let ON_3_ADD: NodeJS.Timeout | undefined;
+      let ON_1_ADD: NodeJS.Timeout | undefined
+      let ON_2_ADD: NodeJS.Timeout | undefined
+      let ON_3_ADD: NodeJS.Timeout | undefined
 
       ON_1_ADD = setTimeout(() => {
-        wrapperRef.current!.classList.add('styles.on1'); // 클래스 조합
-      }, 700);
+        wrapperRef.current!.classList.add(styles.on1) // 클래스 조합
+      }, 700)
 
       ON_2_ADD = setTimeout(() => {
-        wrapperRef.current!.classList.add('styles.on2'); // 클래스 조합
-      }, 1400);
+        wrapperRef.current!.classList.add(styles.on2) // 클래스 조합
+      }, 1400)
 
       ON_3_ADD = setTimeout(() => {
-        wrapperRef.current!.classList.add('styles.on3'); // 클래스 조합
-      }, 2100);
+        wrapperRef.current!.classList.add(styles.on3) // 클래스 조합
+      }, 2100)
 
       return () => {
-        clearTimeout(ON_1_ADD!);
-        clearTimeout(ON_2_ADD!);
-        clearTimeout(ON_3_ADD!);
-      };
+        clearTimeout(ON_1_ADD!)
+        clearTimeout(ON_2_ADD!)
+        clearTimeout(ON_3_ADD!)
+      }
     }
-  }, [wrapperRef]);
-};
+  }, [wrapperRef])
 
+  // isHeaderVisible를 반환
+  return isHeaderVisible
+}
 
 export default useHeadScroll
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,17 @@ import Header from '../components/Header'
 import ScrollTrigger from "gsap/dist/ScrollTrigger"
 import { NextPage } from 'next'
 import styles from '../styles/main.module.scss'
-import useHeadScroll from '../hooks/head';
+import useHeadScroll from '../hooks/head'
 
 gsap.registerPlugin(ScrollTrigger)
+
+
 //function Sub() {
 const Main: NextPage = () => {
   const wrapperRef = useRef<HTMLDivElement | null>(null);
-  useHeadScroll(wrapperRef)
+
+  // useHeadScroll 훅을 호출하고 결과를 변수에 저장
+  const isHeaderVisible = useHeadScroll(wrapperRef);
 
   useEffect(() => {
     document.body.classList.add("stop-scrolling");
@@ -19,27 +23,30 @@ const Main: NextPage = () => {
       document.body.classList.remove("stop-scrolling");
     }, 3000);
   }, []);
+
   return (
     <main className="overflow-x-hidden">
       {/* 헤더 컴포넌트 렌더링 */}
-      {isHeaderVisible && <Header isHeaderVisible={isHeaderVisible} />}
+      <Header isHeaderVisible={isHeaderVisible} />
+
       <div ref={wrapperRef} id="section01" className="section01 h-[var(--vh)] overflow-x-hidden">
         <div className={` ${styles.videoWrapper}`}>
-          <video autoPlay loop muted  >
+          <video autoPlay loop muted>
             <source src="/img/main_video.mp4" type="video/mp4" />
           </video>
         </div>
       </div>
 
-      <div id="section02" className="section02 h-[var(--vh)]">
-        
-      </div>
-      <div id="section03" className="section03 h-[var(--vh)]">
-        
-      </div>
-       
+      <div id="section02" className="section02 h-[var(--vh)]"></div>
+      <div id="section03" className="section03 h-[var(--vh)]"></div>
     </main>
-  )
+  );
 }
 
-export default Main
+export default Main;
+
+
+
+
+
+
