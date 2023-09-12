05f4e853e059798e00095989df65d48f973347fe
nahyunee96 <108210796+nahyunee96@users.noreply.github.com>
2023-09-12 13:24:03 +0900

    Update!

---
+++ src/hooks/typewriter.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+export function TypingEffect(originalText: string, typingSpeed: number, delay: number) {
+  const [text, setText] = useState('');
+  const [isTyping, setIsTyping] = useState(false);
+
+  useEffect(() => {
+    let currentIndex = 0;
+
+    const typingTimeout = setTimeout(() => {
+      setIsTyping(true);
+
+      const typingInterval = setInterval(() => {
+        if (currentIndex <= originalText.length) {
+          setText(originalText.slice(0, currentIndex));
+          currentIndex++;
+        } else {
+          clearInterval(typingInterval);
+        }
+      }, typingSpeed);
+
+      return () => {
+        clearInterval(typingInterval);
+      };
+    }, delay);
+
+    return () => {
+      clearTimeout(typingTimeout);
+    };
+  }, [originalText, typingSpeed, delay]);
+
+  return { text, isTyping };
+}
+
+
+export default TypingEffect;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,24 +5,32 @@ import ScrollTrigger from "gsap/dist/ScrollTrigger"
 import { NextPage } from 'next'
 import styles from '../styles/main.module.scss'
 import useHeadScroll from '../hooks/head'
+import Typewriter from '../hooks/typewriter'
 
 gsap.registerPlugin(ScrollTrigger)
 
 
 //function Sub() {
 const Main: NextPage = () => {
-  const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const wrapperRef = useRef<HTMLDivElement | null>(null)
 
   // useHeadScroll 훅을 호출하고 결과를 변수에 저장
-  const isHeaderVisible = useHeadScroll(wrapperRef);
+  const isHeaderVisible = useHeadScroll(wrapperRef)
 
   useEffect(() => {
-    document.body.classList.add("stop-scrolling");
+    document.body.classList.add("stop-scrolling")
 
     setTimeout(() => {
-      document.body.classList.remove("stop-scrolling");
-    }, 3000);
-  }, []);
+      document.body.classList.remove("stop-scrolling")
+    }, 3000)
+  }, [])
+
+  const originalText = 'WELCOME TO MY HOMEPAGE!'; // 타이핑될 텍스트
+  const typingSpeed = 100; // 타이핑 속도 (밀리초)
+  const delay = 3000; // 시작 지연 시간 (밀리초)
+
+  const { text, isTyping } = Typewriter(originalText, typingSpeed, delay);
+
 
   return (
     <main className="overflow-x-hidden">
@@ -34,19 +42,17 @@ const Main: NextPage = () => {
           <video autoPlay loop muted>
             <source src="/img/main_video.mp4" type="video/mp4" />
           </video>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full text-center text-white text-6xl underline">
+            {isTyping ? <p>{text}</p> : null}
+          </div>
+
         </div>
       </div>
 
       <div id="section02" className="section02 h-[var(--vh)]"></div>
       <div id="section03" className="section03 h-[var(--vh)]"></div>
     </main>
-  );
+  )
 }
 
-export default Main;
-
-
-
-
-
-
+export default Main
